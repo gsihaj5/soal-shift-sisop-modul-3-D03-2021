1ee2a33cb6d7029531e1ac51c212d6bdd45a78c0
widigda_cahya <62281277+widigdacahya@users.noreply.github.com>
2021-05-22 11:00:47 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,78 +1,86 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#include <libgen.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <ctype.h>
+#include <libgen.h>
 
-char *ekstensiFile(char *namaFile) {
-    
-    char *dot = strrchr(namaFile, '.');
-    
-    if(dot==namaFile || !dot) {
-        return "Unknown";
-    }
-    
-    return dot+1;
+
+char *getEkstensiFile(char *filename) {
+    char *dot = strrchr(filename, '.'); //last occurance char
+		// If dot is NULL or dot doesn't exist
+    if(!dot || dot == filename) return "Unknown";
+    return dot + 1;
 }
 
 char *lowerCase(char *nameToLow) {
-    for(char *ch=nameToLow; *ch; ch++) {
-        *ch=tolower(*ch);
+    for(int pos=0; nameToLow[pos]; pos++) {
+        nameToLow[pos] = tolower(nameToLow[pos]);
     }
     return nameToLow;
 }
 
+void createDirIfNotExist(char *dir) {
+	struct stat st = {0};
+
+	if (stat(dir, &st) == -1) {
+			mkdir(dir, 0777);
+	}
+}
+
+
+void *prosesF(void *temp) {
+    char *path;
+    path = (char *)temp;
+    
+    char *namaFile;
+    namaFile= basename(path); //lgens
+
+    char cwd[1000];
+    getcwd(cwd,sizeof(cwd)); //unistd.h
+
+    char namaBaru[1000];
+    char ektensiFile[500];
+
+    strcpy(ektensiFile, getEkstensiFile(namaFile));
+    strcpy(ektensiFile, lowerCase(ektensiFile));
+
+    sprintf(namaBaru, "%s/%s", cwd, ektensiFile);
+    createDirIfNotExist(namaBaru);
+
+    sprintf(namaBaru, "%s/%s", namaBaru,namaFile);
+
+    int statusF = rename(path,namaBaru);
+    if(statusF==0) {
+        printf("Berhasil Dikategorikan\n");
+    } else {
+        printf("Sad, gagal :( \n");
+    }
+
+
+
+}
+
+
 int main(int argc, char* argv[]) {
 
-    /*
-    pthread_t tf;
-                // un,un,fungsi,argumen fungsinya
-    if ( pthread_create(&tf,NULL,&fun1,NULL) != 0 ){
-        return 1;
-    }
-    
-    pthread_join(tf,NULL);
-    */
-
-   if(strcmp (argv[1], "-f")==0 ) {
-       //char *outputFile = basename(argv[0]);
-        int pos;
-        for(pos=2; pos<argc; pos++) {
-            
-            char *dirPath;
-            dirPath = (char*) argv[pos];
-
-            char *fileOperasi;
-            fileOperasi=basename(dirPath);
-
-            char ekstensiFileOperasi[25];
-            strcpy(ekstensiFileOperasi, ekstensiFile(fileOperasi));
-            strcpy(ekstensiFileOperasi, lowerCase(ekstensiFileOperasi));
-
-            char newFileOperasi[1000];
-            char currLoc[1000];
-            getcwd(currLoc,sizeof(currLoc));
-            sprintf(newFileOperasi, "%s/%s", currLoc, fileOperasi );
-
-            //membuat file
-            struct stat dirStat = {0};
-
-            if(stat(newFileOperasi,&dirStat)==-1) {
-                mkdir(newFileOperasi, 0777);
-            }
-
-            sprintf(newFileOperasi, "%s/%s", newFileOperasi, fileOperasi);
-
-            int ket = rename(dirPath, newFileOperasi);
-            if(ket != 0) {
-                printf("Sad, gagal :( \n");
-            } else {
-                printf("Berhasil Dikategorikan\n");
-            }
+    if(strcmp(argv[1], "-f") == 0) {
+        //printf("ini perintah -f\n");
+        int i;
+        for(i=2; i<argc; i++) {
+            prosesF(argv[i]);
         }
-   }
-    
-    return 0;
+        
+
+    }
+
+    if(strcmp(argv[1], "-d") == 0) {
+        printf("ini perintah -d\n");
+        //di modul 2 dirent
+    }
+
+    if(strcmp(argv[1], "*") == 0) {
+        printf("ini perintah *\n");
+    }
+
 }
